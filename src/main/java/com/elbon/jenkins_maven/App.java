60834268006f658563810a0e6e29da79cb10ef1e
chandrahas1993 <chandrahas.chatta@gmail.com>
2019-08-06 23:15:09 +0530

    initial commit

---
+++ src/main/java/com/elbon/jenkins_maven/App.java
@@ -0,0 +1,13 @@
+package com.elbon.jenkins_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Successfully configured and build..." );
+    }
+}
